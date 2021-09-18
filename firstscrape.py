add9daab0d09a18df9cfee1a1950aa5897afd83d
mike-vestergaard <70510215+mike-vestergaard@users.noreply.github.com>
2021-09-18 11:00:20 +0200

    firstscrape created
    first test created

---
+++ firstscrape.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from pathlib import Path
+
+source1 = ""
+source2 = ""
+source3 = ""
+
+# Create a webddriver object for chrome-option and configure
